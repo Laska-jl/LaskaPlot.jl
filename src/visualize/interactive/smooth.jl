112695790c09e27110ee9a38488ce6385d027874
Eliassj <elias.sjolin@gmail.com>
2024-03-25 21:13:16 +0100

    Interactive fourier smoothing

--- src/visualize/interactive/smooth.jl
+++ src/visualize/interactive/smooth.jl
@@ -3,13 +3,23 @@ function interactivefouriersmooth(V::AbstractVector{T},
     σ=1.0,
     m=2,
     σ_range=1:0.1:250,
-    m_range=2:2:50) where {T}
+    m_range=2:2:50;
+    plotoriginal=true) where {T}
     fig = Figure()
-    ax = Axis(fig[1, 1])
-    sl_σ = Slider(fig[1, 2], range=σ_range, startvalue=σ)
-    sl_m = Slider(fig[1, 3], range=m_range, startvalue=m)
-    smoothed = lift(sl_σ, sl_m) do sigma, m
-        fouriersmooth(V, sigma.value, m.value)
+    sl = SliderGrid(
+        fig[2, 1],
+        (label="σ", range=σ_range, startvalue=σ),
+        (label="m", range=m_range, startvalue=m)
+    )
+    smoothed = lift(sl.sliders[1].value, sl.sliders[2].value) do sigma, m
+        LaskaStats.fouriersmooth(V, sigma, m)
     end
-    MakieCore.scatter!(ax, smoothed)
+    ax = Axis(
+        fig[1, 1]
+    )
+    if plotoriginal
+        lines!(V, alpha=0.3, color=:black)
+    end
+    lines!(ax, smoothed)
+    fig
 end
